90b762a21ca774713b342f59c3442d53afd15efa
Zohar Ifrah <cpxzoar@gmail.com>
2023-05-29 21:36:20 +0300

    build updated

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,28 @@ const cors = require('cors')
 
 
 // App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000'
-    ],
-    credentials: true
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
-app.use(express.static('public'))
+
+// const corsOptions = {
+//     origin: [
+//         'http://127.0.0.1:8080',
+//         'http://localhost:8080',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000'
+//     ],
+//     credentials: true
+// }
+// app.use(cors(corsOptions))
+// app.use(express.static('public'))
+
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
 
@@ -166,10 +177,16 @@ app.post('/api/auth/logout', (req, res) => {
 })
 
 
+// Listen will always be the last line in our server!
+// app.listen(3030, () => console.log('Server listening on port 3030!'))
+
+const port = process.env.PORT || 3030
+
 app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-
-// Listen will always be the last line in our server!
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+    console.log('Hello render.com')
+})
